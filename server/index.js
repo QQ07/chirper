6f43b25ca30f1a92dfb970bd46e2f9ab7341652c
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-03-29 02:04:10 +0530

    Basic User Auth Backend

---
+++ server/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js"
+
+// Configurations
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File Storage
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+//Routes with Files
+app.post("/auth/register", upload.single("picture"), register);
+
+// Mongo Setup
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on port http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.error(error));
